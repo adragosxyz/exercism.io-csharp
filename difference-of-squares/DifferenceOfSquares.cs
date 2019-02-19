d69fe94299b79f3b67e2205d81ac05ea08bc6268
XioSC <albastroiudragos@gmail.com>
2019-02-19 23:12:15 +0200

    Added diff-of-squares

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int sum = (max*(max+1))/2;
+        return sum*sum;
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        int sum = (max * (max + 1) * (2*max + 1)) / 6;
+        return sum;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
