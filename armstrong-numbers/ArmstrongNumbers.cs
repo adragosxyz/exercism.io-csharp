2b5bd0e9cdf4a52fd7309df3e8018454335b5600
XioSC <albastroiudragos@gmail.com>
2019-02-19 22:54:41 +0200

    Added armstrong numbers

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string nr = number.ToString();
+        int final=0;
+        for (int i=0;i<nr.Length;i++)
+        {
+            final += (int)Math.Pow(nr[i]-'0', nr.Length);
+        }
+        return final==number;
+    }
+}
