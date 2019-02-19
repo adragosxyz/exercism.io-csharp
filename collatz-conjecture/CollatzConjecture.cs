058106c86b900e8b4c19db10a28b8e0ab6db750c
XioSC <albastroiudragos@gmail.com>
2019-02-19 23:00:51 +0200

    Added collatz

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -2,8 +2,21 @@
 
 public static class CollatzConjecture
 {
+    private static int getSteps(int number)
+    {
+        if (number==1) return 0;
+        if (number%2==0)
+        {
+            return 1+getSteps(number/2);
+        }
+        else 
+        {
+            return 1+getSteps(3*number+1);
+        }
+    }
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if (number<1) throw new ArgumentException("The number must be positive");
+        return getSteps(number);
     }
 }
