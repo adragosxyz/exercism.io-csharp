36e66b87e324099ade905fd71a965fabd0fb54f8
XioSC <albastroiudragos@gmail.com>
2019-03-13 17:24:47 +0200

    Added complex numbers

--- complex-numbers/ComplexNumbers.cs
+++ complex-numbers/ComplexNumbers.cs
@@ -2,52 +2,78 @@ using System;
 
 public struct ComplexNumber
 {
+    private double _Real;
+    private double _Imaginary;
     public ComplexNumber(double real, double imaginary)
     {
+        this._Real = real;
+        this._Imaginary = imaginary;
     }
 
     public double Real()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this._Real;
     }
 
     public double Imaginary()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this._Imaginary;
     }
 
     public ComplexNumber Mul(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        double a = this._Real;
+        double b = this._Imaginary;
+        double c = other.Real();
+        double d = other.Imaginary();
+        double real = a*c - b*d;
+        double imaginary = a*d + b*c;
+        return new ComplexNumber(real, imaginary);
     }
 
     public ComplexNumber Add(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        double real = this._Real + other.Real();
+        double imaginary = this._Imaginary + other.Imaginary();
+        return new ComplexNumber(real, imaginary);
     }
 
     public ComplexNumber Sub(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        double real = this._Real - other.Real();
+        double imaginary = this._Imaginary - other.Imaginary();
+        return new ComplexNumber(real, imaginary);
     }
 
     public ComplexNumber Div(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        double a = this._Real;
+        double b = this._Imaginary;
+        double c = other.Real();
+        double d = other.Imaginary();
+        double real = (a*c+b*d)/(c*c+d*d);
+        double imaginary = (b*c - a*d)/(c*c+d*d);
+
+        return new ComplexNumber(real, imaginary);
     }
 
     public double Abs()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Sqrt(this._Real*this._Real + this._Imaginary*this._Imaginary);
     }
 
     public ComplexNumber Conjugate()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new ComplexNumber(this._Real, -this._Imaginary);
     }
     
     public ComplexNumber Exp()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        double a = this._Real;
+        double b = this._Imaginary;
+        double fact = Math.Pow(Math.E, a);
+        double real = fact * Math.Cos(b);
+        double imaginary = fact * Math.Sin(b);
+        return new ComplexNumber(real, imaginary);
     }
 }
