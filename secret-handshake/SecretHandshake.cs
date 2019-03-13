6c04cb49cb80abdcb2dc29273591cfe66f8685f5
XioSC <albastroiudragos@gmail.com>
2019-03-13 17:53:58 +0200

    Added secret handshake

--- secret-handshake/SecretHandshake.cs
+++ secret-handshake/SecretHandshake.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class SecretHandshake
 {
+    private static readonly Dictionary<int, string> CommandCodes = new Dictionary<int, string>
+    {
+        { 0b1, "wink" },
+        { 0b10, "double blink" },
+        { 0b100, "close your eyes" },
+        { 0b1000, "jump" },
+        { 0b10000, "Reverse" }
+    };
     public static string[] Commands(int commandValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<string> commands = new List<string>();
+        foreach (KeyValuePair<int, string> kvp in CommandCodes)
+        {
+            if ((kvp.Key&commandValue)!=0)
+            {
+                if (kvp.Value!="Reverse") commands.Add(kvp.Value);
+                else commands.Reverse();
+            }
+        }
+        return commands.ToArray();
     }
 }
